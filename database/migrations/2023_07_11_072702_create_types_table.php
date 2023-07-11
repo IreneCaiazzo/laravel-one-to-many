a223b51a75e403c394689e585c734c16818169a8
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-07-11 12:14:22 +0200

    add migrations

--- database/migrations/2023_07_11_072702_create_types_table.php
+++ database/migrations/2023_07_11_072702_create_types_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('projects', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -24,8 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('projects', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('types');
     }
 };
