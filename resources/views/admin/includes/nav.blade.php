c984ab8005b8fac28e52a9964890b69a9312837e
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-07-12 01:21:21 +0200

    tryout show types

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -23,6 +23,15 @@
               <li><a class="dropdown-item" href="{{ route('admin.projects.create') }}">Add</a></li>
             </ul>
           </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Types
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="{{ route('admin.types.index') }}">Index</a></li>
+            </ul>
+          </li>
         
         </ul>
 
---
+++ resources/views/admin/types/create.blade.php
@@ -0,0 +1,88 @@
+@extends('admin.layouts.base')
+
+@section('contents')
+
+    <h1>Add new post</h1>
+
+    {{-- @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif --}}
+
+    <form method="POST" action="{{ route('admin.projects.store') }}" novalidate>
+    @csrf
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input
+                type="text"
+                class="form-control @error('title') is-invalid @enderror"
+                id="title"
+                name="title"
+                value="{{ old('title') }}"
+            >
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="type" class="form-label">Type</label>
+            <select class="form-select" @error('type_id') is-invalid @enderror 
+                id="type" 
+                name="type_id"
+            >
+
+                @foreach($types as $type)
+                <option value="{{ $type->id }}">{{ $type->name }}</option>
+                @endforeach
+
+            </select>
+            @error('type_id')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <input
+                type="text"
+                class="form-control @error('description') is-invalid @enderror"
+                id="description"
+                name="description"
+                value="{{ old('description') }}"
+            >
+            @error('description')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="repo" class="form-label">Repo</label>
+            <textarea
+                class="form-control @error('repo') is-invalid @enderror"
+                id="repo"
+                rows="3"
+                name="repo">{{ old('repo') }}</textarea>
+            @error('repo')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <button class="btn btn-primary">Save</button>
+    </form>
+
+@endsection
